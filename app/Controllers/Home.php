5600fe8a7c30b2ff6a5ac87aa98fa816f4b9404e
meziane1981 <meziane_maamar@yahoo.fr>
2023-05-26 17:32:17 +0200

    4 eme commit le 26MAI23

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,55 +1,155 @@
 <?php
+// namespace App\Controllers;
+
+// class Home extends BaseController
+// {
+//     public function index()
+//     {
+//          return view('welcome_message');
+       
+        
+//     }
+
+//     public function register()
+
+
+//     {
+//      $validation = \Config\Services::validation();
+//      //get 
+//      if ($this->request->getMethod() =="get") 
+//      {
+//           echo view('register');
+//      }
+//      else if($this->request->getMethod() =="post")
+//      {
+//              if($this->validate([
+//                "username"=>"required",
+//                "email"=>"required|valid_email",
+//                "password"=>"required|min_length[5]|max_length[20]",
+//                "cpassword"=>"matches[password]",
+//               ])) {
+//                     // envoyé le formulaire
+//                $username=$this->request->getVar("username");
+//                $email=$this->request->getVar("email");
+//                $password=$this->request->getVar("password");
+//                //$cpassword->$this->request->getVar("cpassword");
+//                           echo "username: $username";
+
+//              }
+//              else{
+//                //return redirect()->back()->withInput();
+//                return redirect()->back()->withInput();
+//                //->with('validation', $validation);
+
+//              }
+           
+
+
+//      }
+        
+//     }
+//     public function login()
+//     {
+//          return view('login');
+       
+        
+//     }
+// }
+
+// namespace App\Controllers;
+
+// class Home extends BaseController
+// {
+//     public function index()
+//     {
+//         return view('welcome_message');
+//     }
+
+//     public function register()
+//     {
+//         if ($this->request->getMethod() == "get") {
+//             echo view('register');
+//         } else if ($this->request->getMethod() == "post") {
+//             $validation = $this->validate([
+//                 "username" => "required",
+//                 "email" => "required|valid_email",
+//                 "password" => "required|min_length[5]|max_length[20]",
+//                 "cpassword" => "matches[password]",
+//             ]);
+
+//             if (!$validation) {
+//                 return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+//             }
+// envoyé le formulaire
+//             $username = $this->request->getVar("username");
+//             $email = $this->request->getVar("email");
+//             $password = $this->request->getVar("password");
+
+//             echo "Username: $username";
+//         }
+//     }
+
+//     public function login()
+//     {
+//         return view('login');
+//     }
+// }
 
 namespace App\Controllers;
+use App\Model\UserModel;
+use App\Models\UserModel as ModelsUserModel;
 
 class Home extends BaseController
 {
     public function index()
     {
-         return view('welcome_message');
-       
-        
+        return view('welcome_message');
     }
 
     public function register()
-
     {
-     //get 
-     if ($this->request->getMethod()=="get") 
-     {
+        $validation = \Config\Services::validation();
 
-          echo view('register');
-     }
-     else if($this->request->getMethod()=="post")
-     {
-             if($this->validate([
-               "username"=>"required",
-               "email"=>"required|valid_email",
-               "password"=>"required|min_length[5]|max_length[20]",
-               "cpassword"=>"matches[password]",
-              ])) {
-                    // envoyé le formulaire
-               $username=$this->request->getVar("username");
-               $email=$this->request->getVar("email");
-               $password=$this->request->getVar("password");
-               //    $cpassword->$this->request->getVar("cpassword");
-                          echo "username: $username";
+        if ($this->request->getMethod() === 'get') {
+            echo view('register', ['validation' => $validation]);
+        } else if ($this->request->getMethod() === 'post') {
+            if ($this->validate([
+                "username" => "required",
+                "email" => "required|valid_email",
+                "password" => "required|min_length[5]|max_length[20]",
+                "cpassword" => "matches[password]",
+            ])) {
+               // envoyé le formulaire
+                $username = $this->request->getVar("username");
+                $email = $this->request->getVar("email");
+                $password = $this->request->getVar("password");
 
-             }
-             else{
-               return redirect()->back()->withInput();
+               // maintenant enregistrez les données ds la BD
 
-             }
-           
+               $data=[
+                    "username"=>$username,
+                    "email"=>$email,
+                    "password"=>$password,
 
+               ];
 
-     }
-        
+               $model= new ModelsUserModel();
+               $model->insert($data);
+
+               $session=session();
+               $session->set("success_message", "l'utilisateur enregistré avec succès");
+               $session->markAsFlashdata("success_message");
+               return view("register");
+
+            } else {
+                return redirect()->back()->withInput()->with('validation', $validation);
+            }
+        }
     }
+
     public function login()
     {
-         return view('login');
-       
-        
+        return view('login');
     }
 }
+
--- app/Views/register.php
+++ app/Views/register.php
@@ -1,44 +1,57 @@
+<?php $validation = session('validation'); ?>
 <?= $this->extend('public_layout')?>
 <?= $this->section('content')?>
 <div class="container">
     <div class="row">
         <div class="col-sm-4 mx-auto">
             <div class="card mt-5">
-
                 <div class="card-body">
                     <h2>Créer un compte</h2>
-                    <?php $validation=\Config\Services::validation();?>
+                    <?php $session=session(); ?>
+                    <?php if(! is_null($session->getFlashdata('success_message'))) ?>
+                    <div class="alert alert-success">
+                    <?= $session->getFlashdata('success_message');?>
+                </div>
+                    
+                    <?php if (isset($validation)): ?>
+                        <div class="alert alert-danger"><?= $validation->listErrors() ?></div>
+                    <?php endif; ?>
                     <form action="<?= base_url('register') ?>" method="post">
                         <div class="mb-2">
                             <label for="username">Pseudo</label>
-                            <input type="text" class="form-control" name="username" value="<?=old("username") ?>">
-                            <div class="text-danger"><?= $validation->getError("username")?></div> 
+                            <input type="text" class="form-control" name="username" value="<?=old("username")?>">
+                            <?php if (isset($validation) && $validation->hasError("username")): ?>
+                                <div class="text-danger"><?= $validation->getError("username")?></div> 
+                            <?php endif; ?>
                         </div>
                         <div class="mb-2">
                             <label for="email">Émail</label>
-                            <input type="email" class="form-control" name="email" value="<?=old("email") ?>">
-                            <div class="text-danger"><?= $validation->getError("email")?></div>
+                            <input type="email" class="form-control" name="email" value="<?=old("email")?>">
+                            <?php if (isset($validation) && $validation->hasError("email")): ?>
+                                <div class="text-danger"><?= $validation->getError("email")?></div>
+                            <?php endif; ?>
                         </div>
                         <div class="mb-2">
                             <label for="password">Mot de Passe</label>
-                            <input type="password" class="form-control" name="password" value="<?=old("password") ?>">
-                            <div class="text-danger"><?= $validation->getError("password")?></div>
+                            <input type="password" class="form-control" name="password" value="<?=old("password")?>">
+                            <?php if (isset($validation) && $validation->hasError("password")): ?>
+                                <div class="text-danger"><?= $validation->getError("password")?></div>
+                            <?php endif; ?>
                         </div>
                         <div class="mb-2">
-                            <label for="cpassword">CConfirme le Mot de Passe</label>
-                            <input type="password" class="form-control" name="cpassword" value="<?=old("cpassword") ?>">
-                            <div class="text-danger"><?= $validation->getError("cpassword")?></div>
+                            <label for="cpassword">Confirme le Mot de Passe</label>
+                            <input type="password" class="form-control" name="cpassword" value="<?=old("cpassword")?>">
+                            <?php if (isset($validation) && $validation->hasError("cpassword")): ?>
+                                <div class="text-danger"><?= $validation->getError("cpassword")?></div>
+                            <?php endif; ?>
                         </div>
                         <div class="mb-2 text-center">
-                        <input type="submit" name ="register" value="Register" class="btn btn-primary">
+                            <input type="submit" name="register" value="Register" class="btn btn-primary">
                         </div>
                     </form>
-
                     <a href="<?= base_url('login') ?>">Vous avez déjà un compte?</a>
-
                 </div>
             </div>
-             
         </div>
     </div>
 </div>
